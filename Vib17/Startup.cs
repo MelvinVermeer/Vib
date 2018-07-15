2b72d72080cdc3fc692bfb1aa1723c8562dd3391
Melvin Vermeer <melvin.vermeer@nike.com>
2018-07-15 20:50:44 +0200

    core 2.1

---
+++ Vib17/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Vib17.Data;
+using Vib17.Services;
+
+namespace Vib17
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication()
+                .AddFacebook(facebookOptions =>
+                {
+                    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                })
+                .AddGoogle(googleOptions => {
+                    googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+                    googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+                })
+                ;
+
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc(options => options.Filters.Add(new RequireHttpsAttribute()));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(WithDefaultRouting);
+
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var db = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                db.Database.Migrate();
+
+                if (!db.Roles.Any(x => x.Name == "Admin"))
+                {
+                    db.Roles.Add(new IdentityRole("Admin"));
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        private void WithDefaultRouting(IRouteBuilder routes)
+        {
+            routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
